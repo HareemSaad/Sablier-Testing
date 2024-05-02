64e2772e1c856779197e1bed31ac2559667c917e
HareemSaad <hareems20014@outlook.com>
2024-05-02 20:28:07 +0500

    dummy token

---
+++ src/Token.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract Token is ERC20, ERC20Pausable, Ownable {
+    constructor() ERC20("Token", "tkn") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Pausable) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
