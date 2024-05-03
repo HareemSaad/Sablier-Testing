d5594ad55efc0c88f9775b6ce8dba99cb81fcf9c
HareemSaad <hareems20014@outlook.com>
2024-05-04 00:52:05 +0500

    sepolia token deploy

--- script/DeployToken_Sepolia.s.sol
+++ script/DeployToken_Sepolia.s.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import "./../src/Token.sol";
 
-contract CounterScript is Script {
+contract TokenDeployScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.broadcast(vm.envUint("SEPOLIA_PK"));
+        new Token();
     }
 }
